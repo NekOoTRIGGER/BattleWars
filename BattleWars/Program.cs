3da5174eff05bada4616d99debcd6e120a2ffeaf
NekOoTRIGGER <harquel.stephane@hotmail.fr>
2024-05-24 00:07:45 +0200

    Typo

--- BattleWars/Program.cs
+++ BattleWars/Program.cs
@@ -7,24 +7,20 @@ namespace BattleWars
     {
         static void Main(string[] args)
         {
-            StartManager startBase = new StartManager();
-
-            startBase.StartMessage();
-            (int rebellesNumber, int empireNumber) sizeTeams = startBase.DefineSizeTeams();
-            string voteResult = startBase.UserVote();
-
+            StartManager startManager = new StartManager();
+            startManager.StartMessage();
+            (int rebellesNumber, int empireNumber) sizeTeams = startManager.DefineSizeTeams();
+            string voteResult = startManager.UserVote();
 
             FightManager fightManager = new FightManager();
             (List<Models.Empire> empires, List<Models.Rebelle> rebelles) teams = fightManager.FillTeamsWithChoice(sizeTeams.empireNumber, sizeTeams.rebellesNumber);
             string finalResult = fightManager.StartFightWithRandomTeam(teams.empires, teams.rebelles);
             fightManager.VerifyTheWinner(voteResult, finalResult);
 
-            if (startBase.Replay())
+            if (startManager.Replay())
             {
                 Main(args);
             }
-            
-
         }
     }
 }
